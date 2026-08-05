14f14eae60866a18d6c6aa1d6ca570e7a604ac80
cgdiazv <cgdiazv@gmail.com>
2026-08-04 20:41:22 -0500

    Fix projects layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import "./globals.css";
+import { PublicFooter } from "@/components/public-footer";
 
 export const metadata: Metadata = {
   title: {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <PublicFooter />
+      </body>
     </html>
   );
 }
